2d6cc8ad18ca6473bb9e999d81edd121562a3c1c
leeungmo <ungmo2@gmail.com>
2016-03-20 18:30:33 +0900

    updated

--- js/main.js
+++ js/main.js
@@ -17,10 +17,10 @@ $('#main-navbar').on('hidden.bs.collapse', function () {
   $(".navbar").removeClass("top-nav-expanded");
 })
 
-// anti drag & copy
-// $(document).ready(function(){
-//   $(document).bind("contextmenu",function(){return false;});
-//   $(document).bind("mousedown",function(){return false;});
-// });
+/* anti drag & copy */
+$(document).ready(function(){
+  $(document).bind("contextmenu",function(){return false;});
+  $(document).bind("mousedown",function(){return false;});
+});
 
 // 2fc73a3a967e97599c9763d05e564189

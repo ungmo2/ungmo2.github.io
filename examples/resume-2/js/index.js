e952d82a2a7a7b50befdd8e42377b95f41622d78
leeungmo <ungmo2@gmail.com>
2016-06-08 14:57:02 +0900

    updated

--- examples/resume-2/js/index.js
+++
@@ -1,9 +0,0 @@
-/*   Just wanted to let you know:
-    This is a WIP :>
-  
-    Disclaimer: This contains incomplete information!
-  
-    Also I'm looking for a job in Berlin or SF in 2015!
-    Send me a mail.
-  
- */

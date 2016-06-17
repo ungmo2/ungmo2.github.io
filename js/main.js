7cf363178a6cef4188355235f5230b000107cd8f
leeungmo <ungmo2@gmail.com>
2016-06-17 13:18:41 +0900

    .

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // Dean Attali / Beautiful Jekyll 2015
 
+var ANTI_DRAG_COPY = true;
+
 // Shorten the navbar after scrolling a little bit down
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
@@ -10,21 +12,24 @@ $(window).scroll(function() {
 });
 
 // On mobile, hide the avatar when expanding the navbar menu
-$('#main-navbar').on('show.bs.collapse', function () {
-  $(".navbar").addClass("top-nav-expanded");
+$('#main-navbar').on('show.bs.collapse', function() {
+    $(".navbar").addClass("top-nav-expanded");
 })
-$('#main-navbar').on('hidden.bs.collapse', function () {
-  $(".navbar").removeClass("top-nav-expanded");
+$('#main-navbar').on('hidden.bs.collapse', function() {
+    $(".navbar").removeClass("top-nav-expanded");
 })
 
 /* anti drag & copy */
-// $(document).ready(function(){
-//   $(document).bind("contextmenu",function(){return false;});
-//   $(document).bind("mousedown",function(){return false;});
-// });
 $(document).ready(function(){
-  $("pre.highlight").bind("contextmenu",function(){return false;});
-  $("pre.highlight").bind("mousedown",function(){return false;});
+  if (ANTI_DRAG_COPY) {
+      $(document).bind("contextmenu",function(){return false;});
+      $(document).bind("mousedown",function(){return false;});
+  }
 });
+
+// $(document).ready(function(){
+//   $("pre.highlight").bind("contextmenu",function(){return false;});
+//   $("pre.highlight").bind("mousedown",function(){return false;});
+// });
 
 // 2fc73a3a967e97599c9763d05e564189

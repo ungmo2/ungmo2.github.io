8845b5175d7110746848fe931fb92662a3fb3615
leeungmo <ungmo2@gmail.com>
2016-07-03 18:12:25 +0900

    updated

--- _posts/questions.js
+++ _posts/questions.js
@@ -683,3 +683,100 @@ function getDayName(a,b){
 }
 
 console.log(getDayName(5,24));
+
+// 답
+function getDayName(month, date) {
+  var dayNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
+  var today = new Date(2016, month - 1, date);
+
+  return dayNames[today.getDay()];
+}
+
+console.log(getDayName(5, 24));
+
+/*
+하샤드 수
+
+하샤드 수는 그 수의 각 자릿수 숫자의 합으로 그 수가 나누어지는 양의 정수를 말한다.
+
+양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 한다. 예를들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수이다.
+
+10, 12, 18, 20, 21, 24, 27, 30, 36, 40, 42, 45, 48, 50, 54, 60, 63, 70, 72, 80, 81, 84, 90, 100, 102, 108, 110, 111, 112, 114, 117, 120, 126, 132, 133, 135, 140, 144, 150, 152, 153, 156, 162, 171, 180, 190, 192, 195, 198, 200
+
+Harshad함수는 양의 정수 n을 매개변수로 입력받는다. 이 n이 하샤드수인지 아닌지 판단하는 함수를 완성하라.
+
+예를들어 n이 10, 12, 18이면 True를 리턴 11, 13이면 False를 리턴한다.
+*/
+
+function isHarshad(n){
+
+}
+
+console.log(Harshad(10)); // true
+console.log(Harshad(12)); // true
+console.log(Harshad(18)); // true
+console.log(Harshad(11)); // false
+console.log(Harshad(13)); // false
+
+// 답
+function isHarshad(n) {
+  var str = n.toString();
+  var sum = 0;
+
+  for (var i = 0; i < str.length; i++) {
+    sum += parseInt(str[i]);
+  }
+
+  // return n % sum ? false : true;
+  return !(n % sum);
+}
+
+console.log(isHarshad(10)); // true
+console.log(isHarshad(12)); // true
+console.log(isHarshad(18)); // true
+console.log(isHarshad(11)); // false
+console.log(isHarshad(13)); // false
+
+
+/*
+두 정수 사이의 합
+
+adder함수는 정수 x, y를 매개변수로 입력받는다.
+두 수와 두 수 사이에 있는 모든 정수를 더해서 리턴하도록 함수를 완성하라.
+
+x와 y가 같은 경우는 둘 중 아무 수나 리턴한다.
+x, y는 음수나 0, 양수일 수 있으며 둘의 대소 관계도 정해져 있지 않다.
+
+예를들어 x가 3, y가 5이면 12를 리턴한다.
+*/
+
+function adder(x, y){
+}
+
+console.log(adder(3, 5)); // 12
+
+// 답
+function adder(x, y) {
+  var sum = 0;
+  var sortedArgs = [];
+
+  if (x > y) {
+    sortedArgs.push(y, x);
+  } else if (x < y) {
+    sortedArgs.push(x, y);
+  } else {
+    return x;
+  }
+
+  for (var i = sortedArgs[0]; i <= sortedArgs[1]; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+console.log(adder(3, 5));  // 12
+console.log(adder(3, 3));  // 3
+console.log(adder(-3, 5)); // 9
+console.log(adder(5, -1)); // 14

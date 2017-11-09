d67b570ba06dd96fc6c4cba666f6108885b2b7c9
ungmo2 <ungmo2@gmail.com>
2017-11-09 10:39:24 +0900

    update

--- javascript/main.js
+++ javascript/main.js
@@ -208,7 +208,8 @@ jQuery(document).ready(function ($) {
   var filter = "win16|win32|win64|mac|macintel"; 
   if ( navigator.platform ) { 
     if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
-      console.log('mobile 접속', $('iframe[src^=https://stackblitz.com]')); 
+      console.log('mobile 접속'); 
+      $('iframe[src^=https://stackblitz.com]').remove();
     } else { 
       console.log('pc 접속'); 
     } 

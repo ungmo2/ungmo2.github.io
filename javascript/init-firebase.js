700e16ede6a6de4d29aea3b28fbf1b4e3c87b87d
ungmo2 <ungmo2@gmail.com>
2020-01-14 22:41:16 +0900

    Create init-firebase.js

---
+++ javascript/init-firebase.js
@@ -0,0 +1,14 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDEIA4IL1-omTI_srsYdYCSyFBSoQkKKMM',
+  authDomain: 'fir-auth-dcc29.firebaseapp.com',
+  databaseURL: 'https://fir-auth-dcc29.firebaseio.com',
+  projectId: 'fir-auth-dcc29',
+  storageBucket: 'fir-auth-dcc29.appspot.com',
+  messagingSenderId: '10148143975',
+  appId: '1:10148143975:web:12388dc751b856b8aa05c2',
+  measurementId: 'G-60HJSDB4CX'
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

9601c4e96980118c6b956e9eed40533487a87307
ungmo2 <ungmo2@gmail.com>
2020-01-15 10:22:32 +0900

    Update auth.js

--- javascript/auth.js
+++ javascript/auth.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
-    console.log(`${user.email} is signed in.`);
+    console.log(`🎉🎉🎉 ${user.email} is signed in. 🎉🎉🎉`);
     document.body.style.display = 'block';
   } else {
     location.href = '/fastcampus/signin';
